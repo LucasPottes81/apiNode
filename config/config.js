c22d731d473fefb7cb957df1d4c115aefb3282ce
Lucas Pottes <2107003l@gmail.com>
2025-05-19 22:16:12 -0300

    ajeitei o .env

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config(); // carrega o .env
+
 module.exports = {
   development: {
-    username: 'root',
-    password: 'Tri21#Luc',
-    database: 'task_manager',
-    host: '127.0.0.1',
-    dialect: 'mysql' 
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
   }
 };
